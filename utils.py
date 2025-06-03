298e88524ebde358cb804b2880a106024da03ce8
yannickRafael <yannickrafael286@gmail.com>
2025-06-03 10:58:30 +0200

    added excel save to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import re
 
+import pandas as pd
+
 
 def extract_hidden_lt(text):
     pattern = r'<input type="hidden" name="lt" value="(.*?)" />'
@@ -22,3 +24,8 @@ def extract_jsessionid(cookies):
         if c.name.lower().startswith("jsessionid"):
             return c.value
     raise Exception("JSESSIONID não encontrado nos cookies.")
+
+def save_to_excel(courses, filename="cursos.xlsx"):
+    df = pd.DataFrame(courses)
+    df.to_excel(filename, index=False)
+    print(f"Arquivo Excel salvo com sucesso como '{filename}'")
