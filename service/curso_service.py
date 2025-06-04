987b098002333059ef5c95aeee0c47bf2e3f5c60
yannickRafael <yannickrafael286@gmail.com>
2025-06-04 13:08:37 +0200

    added script to store cursos

--- service/curso_service.py
+++ service/curso_service.py
@@ -1,27 +1,29 @@
 import psycopg2
 from database import get_db_connection
-from flask import jsonify
 from utils import get_error_message
 
 class Curso_Service:
     
-    def create_curso(cu_nome, cu_code, cu_created_by):
+    def create_curso(cu_nome, cu_code, cu_created_by=0):
 
         """Create a new curso in the database."""
 
         db = get_db_connection()
         try:
+            print(f"type of cu_nome: {type(cu_nome)}, value: {cu_nome}")
+            print(f"type of cu_code: {type(cu_code)}, value: {cu_code}")
+            print(f"type of cu_created_by: {type(cu_created_by)}, value: {cu_created_by}")
             query = "SELECT * FROM create_curso(%s, %s, %s);"
             db.execute(query, (cu_nome, cu_code, cu_created_by))
             result = db.fetchone()
             print(result)
             db.connection.commit()
-            return jsonify({
+            return {
                     "message": "Curso created successfully", 
                     "success": True,
                     "status": 201,
                     "data": result
-                }), 201
+                }
         except psycopg2.Error as e:
             print(str(e))
             message, code = get_error_message(e.diag.message_detail)
@@ -34,30 +36,30 @@ class Curso_Service:
                 except Exception as ex:
                     print(str(ex))
                     db.connection.rollback()
-                    return jsonify({
+                    return {
                         "message": "Could not update existing curso.",
                         "success": False,
                         "status": 500,
                         "data": None
-                    }), 500
+                    }
             else:
                 db.connection.rollback()
-                return jsonify({
+                return {
                     "message": message,
                     "success": False,
                     "status": code,
                     "data": None
-                }), code
+                }
         except Exception as e:
             print(str(e))
             message, code = get_error_message("")
             db.connection.rollback()
-            return jsonify({
+            return {
                 "message": message,
                 "success": False,
                 "status": code,
                 "data": None
-            }), code
+            }
         
     def update_curso(cu_id, cu_nome, cu_code, cu_edited_by):
         """Update an existing curso in the database."""
@@ -69,32 +71,32 @@ class Curso_Service:
             result = db.fetchone()
             print(result)
             db.connection.commit()
-            return jsonify({
+            return {
                     "message": "Curso updated successfully", 
                     "success": True,
                     "status": 200,
                     "data": result
-                }), 200
+                }
         except psycopg2.Error as e:
             print(str(e))
             message, code = get_error_message(e.diag.message_detail)
             db.connection.rollback()
-            return jsonify({
+            return {
                 "message": message,
                 "success": False,
                 "status": code,
                 "data": None
-            }), code
+            }
         except Exception as e:
             print(str(e))
             message, code = get_error_message("")
             db.connection.rollback()
-            return jsonify({
+            return {
                 "message": message,
                 "success": False,
                 "status": code,
                 "data": None
-            }), code    
+            } 
         
     def delete_curso(cu_id, cu_edited_by):
         """Delete an existing curso in the database."""
@@ -106,29 +108,29 @@ class Curso_Service:
             result = db.fetchone()
             print(result)
             db.connection.commit()
-            return jsonify({
+            return {
                     "message": "Curso deleted successfully", 
                     "success": True,
                     "status": 200,
                     "data": result
-                }), 200
+                }
         except psycopg2.Error as e:
             print(str(e))
             message, code = get_error_message(e.diag.message_detail)
             db.connection.rollback()
-            return jsonify({
+            return {
                 "message": message,
                 "success": False,
                 "status": code,
                 "data": None
-            }), code
+            }
         except Exception as e:
             print(str(e))
             message, code = get_error_message("")
             db.connection.rollback()
-            return jsonify({
+            return {
                 "message": message,
                 "success": False,
                 "status": code,
                 "data": None
-            }), code 
+            }
