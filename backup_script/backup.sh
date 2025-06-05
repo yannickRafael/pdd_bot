fb33c0dff4bea0e35a831ff2fd725a2b9963935f
yannickRafael <yannickrafael286@gmail.com>
2025-06-05 16:40:45 +0200

    added files for backup script

---
+++ backup_script/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#replace path with the actual path
+export PGPASSFILE="/path/.pgpass"
+
+
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+
+backup_path="/path/${timestamp}.backup"
+
+/usr/pgsql-17/bin/pg_dump \
+  -U user \
+  -h localhost \
+  -d database \
+  -F c -b -v \
+  -f "$backup_path"
+
+gpg --batch --yes --passphrase "S@fePassw0rd" --symmetric --cipher-algo AES256 "$backup_path"
+rm "$backup_path"
